1bfc67d253d54d518ef1029dc3904618d35be9c6
Misael Taveras <macrot29@gmail.com>
2019-11-09 18:27:50 -0400

    refact: split NavItem

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -1,37 +1,17 @@
 <template>
   <nav class="container">
-    <router-link
-      to="/"
-      class="nav-item t--small"
-      exact-active-class="is-active"
-    >
+    <nav-item to="/" text="Timer">
       <TimerIcon class="icon" />
-      <span class="text">Timer</span>
-    </router-link>
-    <router-link
-      to="/reports"
-      class="nav-item t--small"
-      exact-active-class="is-active"
-    >
+    </nav-item>
+    <nav-item to="/reports" text="Reports">
       <ReportIcon class="icon" />
-      <span class="text">Reports</span>
-    </router-link>
-    <router-link
-      to="/settings"
-      class="nav-item t--small"
-      exact-active-class="is-active"
-    >
+    </nav-item>
+    <nav-item to="/settings" text="Settings">
       <SettingsIcon class="icon" />
-      <span class="text">Settings</span>
-    </router-link>
-    <router-link
-      to="/profile"
-      class="nav-item t--small"
-      exact-active-class="is-active"
-    >
+    </nav-item>
+    <nav-item to="/profile" text="Settings">
       <AccountIcon class="icon" />
-      <span class="text">Settings</span>
-    </router-link>
+    </nav-item>
   </nav>
 </template>
 
@@ -40,9 +20,10 @@ import TimerIcon from '../assets/icons/timer-icon.svg'
 import ReportIcon from '../assets/icons/report-icon.svg'
 import SettingsIcon from '../assets/icons/settings-icon.svg'
 import AccountIcon from '../assets/icons/account-icon.svg'
+import NavItem from './NavItem.vue'
 
 export default {
-  components: { TimerIcon, ReportIcon, SettingsIcon, AccountIcon },
+  components: { NavItem, TimerIcon, ReportIcon, SettingsIcon, AccountIcon },
 }
 </script>
 
@@ -56,49 +37,5 @@ export default {
   grid-template-columns: repeat(4, 1fr);
   height: 58px;
   padding: 0 32px;
-}
-
-.nav-item {
-  display: block;
-  padding: 0.25em 0.5em;
-  text-decoration: none;
-}
-.nav-item:hover,
-.nav-item:active,
-.nav-item:visited,
-.nav-item:focus {
-  text-decoration: none;
-}
-
-.nav-item .text {
-  display: none;
-}
-
-.nav-item .icon {
-  display: inline-block;
-  text-align: center;
-  vertical-align: middle;
-  max-width: 20px;
-}
-.nav-item .icon .cls-1 {
-  fill: rgba(0, 0, 0, 0.35);
-  stroke: transparent;
-}
-
-.nav-item.is-active {
-  padding-left: 2em;
-  padding-right: 2em;
-}
-
-.nav-item.is-active .text {
-  display: block;
-  margin-top: 4px;
-}
-.nav-item.is-active .icon {
-  max-width: 24px;
-}
-.nav-item.is-active .icon .cls-1 {
-  fill: #fff;
-  stroke: transparent;
 }
 </style>
---
+++ src/components/NavItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <router-link
+    :to="to"
+    class="nav-item t--small"
+    exact-active-class="is-active"
+  >
+    <slot></slot>
+    <span class="text">{{ text }}</span>
+  </router-link>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class NavItem extends Vue {
+  @Prop() private to!: string
+  @Prop() private text!: string
+}
+</script>
+
+<style scoped>
+.nav-item {
+  display: block;
+  padding: 0.25em 0.5em;
+  text-decoration: none;
+}
+.nav-item:hover,
+.nav-item:active,
+.nav-item:visited,
+.nav-item:focus {
+  text-decoration: none;
+}
+
+.nav-item .text {
+  display: none;
+}
+
+.nav-item >>> .icon {
+  display: inline-block;
+  text-align: center;
+  vertical-align: middle;
+  max-width: 20px;
+}
+.nav-item >>> .icon .cls-1 {
+  fill: rgba(0, 0, 0, 0.35);
+  stroke: transparent;
+}
+
+.nav-item.is-active {
+  padding-left: 2em;
+  padding-right: 2em;
+}
+
+.nav-item.is-active .text {
+  display: block;
+  margin-top: 4px;
+}
+.nav-item.is-active >>> .icon {
+  max-width: 24px;
+}
+.nav-item.is-active >>> .icon .cls-1 {
+  fill: #fff;
+  stroke: transparent;
+}
+</style>
