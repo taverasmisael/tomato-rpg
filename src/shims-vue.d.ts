06b40e051100359c45c21d5a94fac2b13f1150d5
Misael Taveras <macrot29@gmail.com>
2019-11-09 14:33:41 -0400

    feat: add types

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,22 @@
-declare module "*.vue" {
-  import Vue from "vue";
-  export default Vue;
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
+
+declare module '*.svg' {
+  import Vue from 'vue'
+  class SVGIcon extends Vue {}
+  export default SVGIcon
+}
+
+declare module 'vue-100vh' {
+  import { Component, Prop } from 'vue-property-decorator'
+  import Vue from 'vue'
+
+  @Component
+  class vue100vh extends Vue {
+    @Prop() css: string
+  }
+
+  export default vue100vh
 }
