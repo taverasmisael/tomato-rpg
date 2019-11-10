71ae924529a13581d303d1809296831744aad65a
Misael Taveras <macrot29@gmail.com>
2019-11-10 19:49:56 -0400

    feat: create base for timer (input and screen)

---
+++ src/components/TaskInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <label for="taskNameInput" class="container">
+    <span class="t--placeholder label">Task name</span>
+    <input
+      autocomplete="off"
+      id="taskNameInput"
+      name="taskNameInput"
+      class="input"
+      placeholder="Task name"
+      :value="value"
+      @input="input"
+  /></label>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class TaskINput extends Vue {
+  @Prop() input!: Function
+  @Prop() value!: String
+}
+</script>
+
+<style scoped>
+.container {
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  display: block;
+  margin: 4px 0;
+  padding: 4px;
+  position: relative;
+}
+
+.label {
+  opacity: 0;
+  position: absolute;
+  z-index: -1;
+}
+
+.input,
+.input::placeholder {
+  appearance: none;
+  background: transparent;
+  border: 0;
+  display: block;
+  outline: none;
+  padding: 0;
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 1em;
+  line-height: 1.19;
+  padding: 8px;
+  text-align: left;
+}
+
+.input::placeholder {
+  padding: 0;
+}
+</style>
---
+++ src/views/Timer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="timer screen">
+    <h1 class="t-task--label">
+      Resting from:
+      <strong class="t-task--name">{{ taskName || 'Task name' }}</strong>
+    </h1>
+
+    <TaskInput :input="changeTaskName" :value="taskName" />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import TaskInput from '@/components/TaskInput.vue'
+
+@Component({ components: { TaskInput } })
+export default class TimerScreen extends Vue {
+  taskName = ''
+
+  changeTaskName({ target }: any) {
+    const taskName = target.value as string
+    this.taskName = taskName
+  }
+}
+</script>
